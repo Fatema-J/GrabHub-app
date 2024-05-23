a7e9a8a0abb14978e637ca0885e6936362e4adfc
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-05-23 08:14:00 -0700

    mm

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,13 @@
+const { name } = require('ejs');
 const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
-
+const userSchema = new Schema({
+  name: string
+}, {
+  timestamps:true
+})
+module.exports = mongoose.model('User', userSchema);
 const dishSchema = new Schema ({
   Item: {
     type: String,

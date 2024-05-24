21bf99e6f5f340b54cde2bf6c3e7be862c1dd306
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-05-24 05:57:48 -0700

    sever

--- controllers/reciept.js
+++ controllers/reciept.js
@@ -1,5 +1,17 @@
-const reciept = require('../models/reciept')
+const reciept = require('../models/reciept') 
+const dish = require('../models/dish')
+const addtoDB = async(req, res) => {
+try{
+const dish = await Dish.findById(req.params.id);
+reciept.resturant.push(req.body.performerId);
+await reciept.save()
+res.redirect(`/reciept/${reciept._id}`);
+} catch(error){
+  console.log(error);
+    res.redirect('/reciept');
+}
+}
 
 module.exports = {
-  
+  addtoDB
 }
--- routes/reciept.js
+++ routes/reciept.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const recieptCtrl = require('../controllers/reciept');
 
 router.get('/', recieptCtrl.index);
+router.get('/views/user', recieptCtrl.index)
 
 module.exports = router;
 

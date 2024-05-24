f9ad0914724c778300ac4a3cfd19e0f46c22bd3b
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-05-24 10:48:43 +0300

    Database & Rest seed done

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config();
 
 require('dotenv').config();
 require('./config/database');

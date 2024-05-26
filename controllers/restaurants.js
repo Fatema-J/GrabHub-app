ab2dfe71116272752f52c89ca8ae1d967031ec0d
Fatem J <fatima.aljunaid.257@gmail.com>
2024-05-26 21:50:36 +0300

    made dishes view

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,4 +1,5 @@
 const Restaurant = require('../models/restaurant')
+const Dish = require('../models/dish')
 const mongodb = require('mongodb')
 const { ObjectId } = mongodb
 
@@ -9,17 +10,28 @@ const index = async (req, res) => {
 }
 
 const show = async (req, res) => {
-  const restaurant = await Restaurant.findById(req.params.id)
+  const restaurant = await Restaurant.findById(req.params.id).populate('menu')
 
-  const allCategories = restaurant.menu.map((dish) => dish.category)
-  //keep distinct categories only
-  const categories = Array.from(new Set(allCategories))
-  console.log('categories', categories)
-  const dishes = restaurant.menu
-  console.log(restaurant.menu);
+  const dishes = await Dish.find({});
+
+  const restaurantMenu = restaurant.menu;
+  console.log(restaurantMenu)
+//   const restaurantDishes = restaurantMenu.map(dish => {
+//     return {name: dish.Item, 
+//             category: dish.category,  
+//             description: dish.description,
+//             price: dish.price}
+// )};
+
+  // const allCategories = restaurant.menu.map((dish) => dish.category)
+  // //keep distinct categories only
+  // const categories = Array.from(new Set(allCategories))
+  // console.log('categories', categories)
+  // const dishes = restaurant.menu
+  // console.log(restaurant.menu);
   
 
-  res.render('restaurants/show', { title: restaurant.name, restaurant, categories })
+  res.render('restaurants/show', { title: restaurant.name, restaurant, restaurantMenu })
 }
 
 module.exports = {

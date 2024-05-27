43bd93d88a39c5fd1470bfcfe0b067239ffa35cc
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-05-27 23:11:20 +0300

    fixed user can view basket only

--- controllers/baskets.js
+++ controllers/baskets.js
@@ -1,5 +1,5 @@
 const Basket = require('../models/basket')
-
+const Dish = require('../models/dish')
 const add = async (req, res) => {
   const basket = await Basket.find({})
   res.render('basket/add'),
@@ -8,7 +8,6 @@ const add = async (req, res) => {
       basket
     }
 }
-
 //delete, +if statement
 async function deleteBasket(req, res) {
   try {
@@ -18,19 +17,27 @@ async function deleteBasket(req, res) {
     console.log(err)
   }
 }
-
 const show = async (req, res) => {
   try {
-    const baskets = await Basket.find({})
+    const baskets = await Basket.findById('6654314fe9c6c5b6fea3d9a1')
+    console.log(baskets.orderedItems)
+    const basketdish = []
+    baskets.orderedItems.forEach(async (item) => {
+      console.log(item.dish)
+      basketdish.push(await Dish.findById(item.dish))
+    })
+    //await Dish.findById(baskets.orderedItems[0].dish)
+    console.log('=============================')
+    console.log(basketdish)
     res.render('baskets/show', {
       title: 'Basket',
-      baskets
+      baskets,
+      basketdish
     })
   } catch (err) {
     console.log(err)
   }
 }
-
 // update the basket, +if statement
 async function updateBasket(req, res) {
   const basket = await Basket.findById({})
